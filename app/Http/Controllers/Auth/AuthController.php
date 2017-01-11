ed08a33d330a8789c92f656208a1becfe3959fbb
Sidrit Trandafili <forge405@gmail.com>
2017-01-11 11:31:46 +0100

    More

--- app/Http/Controllers/Auth/AuthController.php
+++ app/Http/Controllers/Auth/AuthController.php
@@ -37,6 +37,7 @@ class AuthController extends Controller
     public function __construct()
     {
         view()->share('navCategories', Category::whereActive(1)->orderBy('id', 'ASC')->get());
+        view()->share('siteSettings', Settings::first());
 
         $this->middleware('guest', ['except' => 'getLogout']);
     }
